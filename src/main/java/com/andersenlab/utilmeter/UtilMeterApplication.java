ae9c9f9777439c2e45f30cc90df21bf13eea3e4c
Samir <sa.alizada@andersenlab.com>
2026-04-15 00:03:28 +0400

    initial commit

---
+++ src/main/java/com/andersenlab/utilmeter/UtilMeterApplication.java
@@ -0,0 +1,13 @@
+package com.andersenlab.utilmeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilMeterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtilMeterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andersenlab/utilmeter/UtilMeterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andersenlab.utilmeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilMeterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
